aea4808ed8e3897b480ad3e226717070117cd14e
Aleksa <aleksacolic01@gmail.com>
2024-10-05 21:44:33 +0200

    Added scratching mechanic

--- Scripts/bite_mark.gd
+++ Scripts/bite_mark.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var damage: float = 0.5
-
+var scratches_left: int = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,3 +10,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	StatsManager.ReduceHealth(damage * delta)
+
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Hands"):
+		scratches_left -= 1
+		if(scratches_left <= 0):
+			queue_free()
+		print("Mark scratches left: ",scratches_left)
--- Scripts/hand.gd
+++ Scripts/hand.gd
@@ -26,6 +26,7 @@ func _ready():
 	if !move_with_keys:
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	print(active)
+	
 
 
 func _input(event):
