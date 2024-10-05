d0d3dfd88437e26d767964e35b012a103039d242
BYK27 <vuk.aleksijevic2001@gmail.com>
2024-10-05 18:26:24 +0200

    Hand switch

---
+++ Scripts/world.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var hand_right: Hand = $HandRight
+@onready var hand_left: Hand = $HandLeft
+
+var current_hand: Hand = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_active_hand(hand_right)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("switch_hand"):
+		switch_hand()
+
+func set_active_hand(hand: Hand) -> void:
+	hand_right.active = false
+	hand_left.active = false
+	
+	current_hand = hand
+	current_hand.active = true
+	
+	print(hand_left.active)
+	print(hand_right.active)
+	
+func switch_hand() -> void:
+	if current_hand == hand_left:
+		set_active_hand(hand_right)
+	elif current_hand == hand_right:
+		set_active_hand(hand_left)
