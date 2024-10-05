fb4c4cfd0180665561777335ce49eebb8b29e087
Milos Delic <milosde02@gmail.com>
2024-10-05 17:15:14 +0200

    hit box hurt box collisions su namsteni mudja pomogo

--- Scripts/human.gd
+++ Scripts/human.gd
@@ -6,6 +6,5 @@ signal mosquito_overlapped_end
 func _on_hurt_box_area_entered(area):
 	emit_signal("mosquito_overlapped_start")
 
-
 func _on_hurt_box_area_exited(area):
 	emit_signal("mosquito_overlapped_end")
