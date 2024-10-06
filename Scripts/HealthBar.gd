0fdeeba6b09fbee745383b58d042a7e46182b726
Aleksa <aleksacolic01@gmail.com>
2024-10-06 12:35:42 +0200

    added shoulder buttons for switching hands, blood bar goes from 0 to 100 now

--- Scripts/HealthBar.gd
+++ Scripts/HealthBar.gd
@@ -3,12 +3,12 @@ extends ProgressBar
 #signal on_damaged(damage_amount)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	value = StatsManager.GetHealth()
+	value = 100 - StatsManager.GetHealth()
 	StatsManager.connect("on_health_change",_on_health_changed)
 
 
 func _on_health_changed() -> void:
-	value = StatsManager.GetHealth()
+	value = 100 - StatsManager.GetHealth()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
