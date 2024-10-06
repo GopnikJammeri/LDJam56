af04138217f3d0e233606c7cf6be2841a6e14968
Milos Delic <milosde02@gmail.com>
2024-10-06 20:57:11 +0200

    rules and tips screen

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -5,3 +5,7 @@ func _on_play_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_rules_and_controls_pressed():
+		get_tree().change_scene_to_file("res://Scenes/rules_and_controles.tscn")
